41f88072e6509bf94b66121ec77c6624da54620a
romain sanson <romainsanson.gm@gmail.com>
2019-03-06 12:18:30 +0100

    fix paperclip thumb & recipes index

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,6 @@ class Recipe < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
 
-  # include Paperclip::Glue
-  has_attached_file :image, styles: { medium: "400x400#>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
